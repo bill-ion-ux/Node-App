763c4adbe77e47e945d738d82f1a5ca2c1033475
iamnabil <23001886@siswa.um.edu.my>
2025-08-24 21:51:31 +0800

    update server

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const path = require('path');
 const posts = require('./routes/posts');
+const logger = require('./middleware/logger');
+const errorHandler = require('./middleware/error');
+const { default: notFound } = require('./middleware/notFound');
+const port = process.env.PORT || 8000;
+
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+
+// logger middleware
+app.use(logger);
+
+
+//Routes
+app.use('/api/posts', posts);
+
+app.use(notFound);
+
+// Error Handler
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`server is running on port ${port}`));
